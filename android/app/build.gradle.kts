e932bdf84fabf47163d0610acaa7f3a54d2404dd
rube123 <21030305@itcelaya.edu.mx>
2025-11-18 23:21:02 -0600

    AArregle formato build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-    }
+    } 
 
     buildTypes {
         release {
